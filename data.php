b85c8af0da53be48028e885487d6c79435695ab6
Irakli Kereselidze <rmgwork9@gmail.com>
2025-05-15 12:19:58 +0400

    Init

---
+++ data.php
@@ -0,0 +1,198 @@
+<?php
+
+// --- General Page Data ---
+$page_config = [
+    'title' => 'Monica',
+    'favicon_url' => 'https://themewagon.github.io/monica/favicon-32x32.png',
+    'logo_url' => 'https://themewagon.github.io/monica/images/logo.svg',
+    'logo_alt' => 'monica logo',
+    'home_link' => './index.php',
+];
+
+// --- Navigation ---
+$nav_items = [
+    ['text' => 'About', 'url' => '#about'],
+    ['text' => 'Services', 'url' => '#services'], 
+    ['text' => 'Journal', 'url' => '#journal'],   
+    ['text' => 'Contact', 'url' => '#contact'],  
+];
+
+$cta_button = [
+    'text' => "Let's Work Together",
+    'url' => '#contact-form', 
+];
+
+// --- Intro Section ---
+$intro_section_data = [
+    'greeting' => "Hello, I'm Monica",
+    'headline' => "I create marketing strategies for your business that get results.",
+    'scroll_target_id' => "about",
+    'scroll_text' => "Scroll for more",
+    'main_image_url' => "https://themewagon.github.io/monica/images/intro-bg.jpg",
+    'main_image_alt' => "Monica",
+    'background_image_url' => "https://themewagon.github.io/monica/images/geometric_shape.svg",
+    'background_image_alt' => "geometric_shape",
+];
+
+// --- About Section ---
+$about_section_data = [
+    'pre_title' => 'About',
+    'title' => 'An inspiring headline about yourself.',
+    'more_about_me_link_text' => 'More About Me',
+    'more_about_me_link_url' => '#', 
+    'paragraphs' => [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+        "Autem tenetur commodi maiores. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est eligendi fugit, facilis velit reiciendis iure laudantium. Praesentium repellat corrupti dolor sit sint obcaecati. Modi aut quo molestiae a explicabo maiores necessitatibus nemo repellendus architecto? Corrupti numquam ullam nostrum, eveniet at doloribus blanditiis aliquid a est porro aspernatur pariatur culpa soluta nesciunt odio quasi maxime debitis illum.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+    ],
+    'social_links' => [
+        ['icon_class' => 'fa-brands fa-square-facebook', 'url' => '#0'],
+        ['icon_class' => 'fa-brands fa-twitter', 'url' => '#0'],
+        ['icon_class' => 'fa-brands fa-instagram', 'url' => '#0'],
+        ['icon_class' => 'fa-solid fa-basketball', 'url' => '#0'],
+    ],
+];
+
+// --- Expertise Section ---
+$expertise_section_data = [
+    'pre_title' => 'Expertise',
+    'title' => 'My key areas of expertise.',
+    'main_text' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum suscipit debitis quam dignissimos veritatis atque pariatur magnam obcaecati fugit reprehenderit vel numquam facere esse est deserunt, perferendis commodi voluptatem similique.',
+    'view_all_link_text' => 'View All Services',
+    'view_all_link_url' => '#', 
+    'items' => [
+        [
+            'title' => 'Digital Marketing',
+            'text' => 'Quibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.'
+        ],
+        [
+            'title' => 'Project Management',
+            'text' => 'Rerum quam quos. Quibusdam quis autem voluptatibus earum vel ex error ea magni. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.'
+        ],
+        [
+            'title' => 'Content Marketing',
+            'text' => 'Eaque velit eligend. iQuibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.'
+        ],
+        [
+            'title' => 'Research & Discovery',
+            'text' => 'Quibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.'
+        ],
+    ]
+];
+
+// --- Clients Section ---
+$clients_section_data = [
+    'pre_title' => 'Clients',
+    'title' => 'I have had the privilege of working with these incredible brands.',
+    'paragraphs' => [
+        "Quibusdam quis autem voluptatibus earum vel ex error ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum suscipit debitis quam dignissimos veritatis atque pariatur magnam obcaecati fugit reprehenderit vel numquam facere esse est deserunt, perferendis commodi voluptatem similique.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, numquam molestiae vel quaerat quas facilis voluptates rerum aspernatur quam voluptatem ea, vitae illo, omnis minus vero minima maiores quia nihil incidunt provident debitis ab qui quasi. Iure unde numquam in nulla praesentium nesciunt dolore exercitationem, odit expedita minima quisquam ullam ex. Aut perferendis vel consectetur modi esse. Temporibus reprehenderit alias magni atque repellat aspernatur voluptates, accusantium pariatur libero ad nesciunt illum labore facere. Earum iure consequatur cumque omnis maiores optio."
+    ],
+    'logos' => [
+        ['src' => 'https://themewagon.github.io/monica/images/clients/flash.svg', 'alt' => 'flash', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/rise.svg', 'alt' => 'rise', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/cactus.svg', 'alt' => 'cactus', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/pinpoint.svg', 'alt' => 'pinpoint', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/proline.svg', 'alt' => 'proline', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/vision.svg', 'alt' => 'vision', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/volume.svg', 'alt' => 'volume', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/hitech.svg', 'alt' => 'hitech', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/terra.svg', 'alt' => 'terra', 'url' => '#'],
+        ['src' => 'https://themewagon.github.io/monica/images/clients/chain.svg', 'alt' => 'chain', 'url' => '#'],
+    ]
+];
+
+// --- Testimonials Section ---
+$testimonials_data = [
+    [
+        'text' => 'Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. Voluptas tempore rem. Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil nostrum. Laudantium quia consequatur molestias.',
+        'image_url' => 'https://themewagon.github.io/monica/images/avatars/user-01.jpg',
+        'name' => 'John Rockfeller',
+        'company' => 'Standard Oil Co.'
+    ],
+    [
+        'text' => 'Nisi dolores quaerat fuga rem nihil nostrum. Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. Voluptas tempore rem. Laudantium quia consequatur molestias. Explicabo a quaerat sint autem dolore ducimus ut consequatur neque.',
+        'image_url' => 'https://themewagon.github.io/monica/images/avatars/user-04.jpg',
+        'name' => 'Andrew Carnegie',
+        'company' => 'Carnegie Steel Co.'
+    ],
+    [
+        'text' => 'Voluptas tempore rem. Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil nostrum. Laudantium quia consequatur molestias.',
+        'image_url' => 'https://themewagon.github.io/monica/images/avatars/user-06.jpg',
+        'name' => 'Henry Ford',
+        'company' => 'Ford Motor Co.'
+    ],
+    [
+        'text' => 'Molestiae incidunt consequatur quis ipsa autem nam sit enim magni. Voluptas tempore rem. Explicabo a quaerat sint autem dolore ducimus ut consequatur neque. Nisi dolores quaerat fuga rem nihil nostrum. Laudantium quia consequatur molestias.',
+        'image_url' => 'https://themewagon.github.io/monica/images/avatars/user-02.jpg',
+        'name' => 'John Morgan',
+        'company' => 'JP Morgan & Co.'
+    ],
+];
+
+// --- Get Started Section ---
+$get_started_section_data = [
+    'title' => 'Get started with a consultation today.',
+    'text' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis rem, esse doloribus sint eaque at debitis enim vitae minus expedita ratione dignissimos sit nostrum optio sequi. Ipsa at beatae quam.',
+    'button_text' => "Let's Work Together",
+    'button_url' => '#',
+];
+
+// --- Recent Articles Section ---
+$recent_articles_section_data = [
+    'pre_title' => 'Recent Articles',
+    'title' => 'Latest articles from my blog.',
+    'articles' => [
+        [
+            'tag' => 'Sponsored',
+            'tag_link' => '#',
+            'title' => 'Need Web Hosting for Your Websites?',
+            'title_link' => '#',
+            'content_html' => 'Need hosting for your website? We would highly recommend <a href="#"><span>DreamHost.</span></a> Enjoy 100% in-house support, guaranteed performance and uptime, 1-click installs, and a super-intuitive control panel to make managing your websites and projects easy. <a href="#"><span>Sign up</span></a> now to get started.'
+        ],
+        [
+            'tag' => 'Productivity',
+            'tag_link' => '#',
+            'title' => '5 Marketing Productivity Apps for Your Team',
+            'title_link' => '#',
+            'content_html' => 'Rerum quam quos. Quibusdam quis autem voluptatibus earum vel ex error ea magni. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.'
+        ],
+        [
+            'tag' => 'SEO',
+            'tag_link' => '#',
+            'title' => '7 SEO Factors to Consider for Better Website Ranking',
+            'title_link' => '#',
+            'content_html' => 'Quibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.'
+        ],
+        [
+            'tag' => 'Productivity',
+            'tag_link' => '#',
+            'title' => '5 Effective Web Design Principles',
+            'title_link' => '#',
+            'content_html' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum ullam et laborum aperiam. Dignissimos quae voluptatibus ullam esse sapiente fugiat, rerum labore cupiditate obcaecati dolore necessitatibus id magni rem officia dolor nam sit laudantium. Quo eaque eveniet quibusdam unde.'
+        ],
+    ]
+];
+
+// --- Footer Data ---
+$footer_data = [
+    'about_monica_title' => 'About Monica',
+    'about_monica_text' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illum quasi facere libero, fugiat laboriosam possimus reprehenderit eveniet vero voluptatum fugit ad quis veritatis suscipit beatae incidunt perferendis tempore earum hic repellendus quisquam recusandae ipsa id esse. Nobis, aut deleniti. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere ratione dignissimos.',
+    'site_links_title' => 'Site Links',
+    'site_links' => [
+        ['text' => 'Home', 'url' => $page_config['home_link']],
+        ['text' => 'Services', 'url' => '#services'],
+        ['text' => 'Journal', 'url' => '#journal'],
+        ['text' => 'About', 'url' => '#about'],
+        ['text' => 'Contact', 'url' => '#contact'],
+        ['text' => 'DreamHost', 'url' => '#'],
+    ],
+    'newsletter_title' => 'Newsletter',
+    'newsletter_text' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere ratione dignissimos.',
+    'social_links' => $about_section_data['social_links'], 
+    'copyright_text' => '© Copyright Monica 2022',
+    'design_by_html' => 'Design by <a href="#">StyleShout</a>',
+    'distribution_by_html' => 'Distribution By <a href="#">ThemeWagon</a>',
+];
+
+?>
